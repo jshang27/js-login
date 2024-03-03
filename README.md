00891c68530d461449dfed51648dc2d07f94647d
Jason Shang <jshang27@concordcarlisle.org>
2024-03-02 21:18:00 -0500

    organised html and css into it's own folder, added a timeout and max request amount (that dubiously works?)

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ JavaScript log-in server.
 - [W3 Schools Node.js `http` Module](https://www.w3schools.com/nodejs/nodejs_http.asp)
 - [MDN Web Docs `URL`](https://developer.mozilla.org/en-US/docs/Web/API/URL)
 - [MDN Web Docs `URLSearchParams`](https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams)
+- [MDN Web Docs HTTP Status Codes](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import http from "http";
 import fs from "fs";
 
+/* SERVER START UP */
 if (process.argv.length < 3) {
     console.error("expected a port to serve on in the second argument");
     process.exit(1);
@@ -10,29 +11,62 @@ if (Number.isNaN(+process.argv[2])) {
     process.exit(1);
 }
 
-http.createServer(function (req, res) {
+/* SERVER */
+function send_timeout(res: http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage; }): void {
+    res.writeHead(408, { "Content-Type": "text/html" });
+    res.write(fs.readFileSync("pages/error/timeout.html"));
+}
+
+let currentRequestPool = 0;
+
+http.createServer(async (req, res) => {
+    if (currentRequestPool > 20) {
+        res.writeHead(503, { "Content-Type": "text/html" });
+        res.write(fs.readFileSync("pages/error/503.html"));
+        res.end();
+        return;
+    }
+    currentRequestPool++;
     let u = new URL(`https://${req.headers.host}${req.url}`);
     const file = u.pathname.substring(1);
     if (file == "signup") {
-        handleSignup(res, u.searchParams);
+        await handleSignup(res, u.searchParams);
     } else if (file == "login") {
-        handleLogin(res, u.searchParams);
-    } else if (file == "error.css") {
+        await handleLogin(res, u.searchParams);
+    } else if (file == "error/error.css") {
         res.writeHead(200, { "Content-Type": "text/css" });
-        res.write(fs.readFileSync("src/error.css"));
+        res.write(fs.readFileSync("pages/error/error.css"));
     } else {
         res.writeHead(404, { "Content-Type": "text/html" });
-        res.write(fs.readFileSync("src/404.html"))
+        res.write(fs.readFileSync("pages/error/404.html"));
     }
     res.end();
+    currentRequestPool--;
 }).listen(process.argv[2])
 
-function handleSignup(res: http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage; }, searchParams: URLSearchParams) {
-    res.writeHead(501, { "Content-Type": "text/html" });
-    res.write(fs.readFileSync("src/server-error.html"));
+async function handleSignup(res: http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage; }, searchParams: URLSearchParams) {
+
+    return new Promise<void>((resolve, reject) => {
+        let timerID = setTimeout(() => {
+            send_timeout(res);
+            reject()
+        }, 500);
+        res.writeHead(501, { "Content-Type": "text/html" });
+        res.write(fs.readFileSync("pages/error/server-error.html"));
+        clearTimeout(timerID);
+        resolve();
+    })
 }
 
-function handleLogin(res: http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage; }, searchParams: URLSearchParams) {
-    res.writeHead(501, { "Content-Type": "text/html" });
-    res.write(fs.readFileSync("src/server-error.html"));
+async function handleLogin(res: http.ServerResponse<http.IncomingMessage> & { req: http.IncomingMessage; }, searchParams: URLSearchParams) {
+    return new Promise<void>((resolve, reject) => {
+        let timerID = setTimeout(() => {
+            send_timeout(res);
+            reject();
+        }, 500);
+        res.writeHead(501, { "Content-Type": "text/html" });
+        res.write(fs.readFileSync("pages/error/server-error.html"));
+        clearTimeout(timerID);
+        resolve();
+    })
 }
